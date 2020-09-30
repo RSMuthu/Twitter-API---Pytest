15289007c984ada5e65b669f44ddef3e09c77eaf
Muthu Kumaran R <RSMuthu@users.noreply.github.com>
2020-09-30 19:58:51 +0530

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import pytest, json
+from requests_oauthlib import OAuth1Session
+
+BASE_URL = "https://api.twitter.com/1.1" ## base URL for Twitter API communication
+## We will be using Twitter API 1.1 for the API activities
+
+def __load_tokens():
+    '''
+    private method not to be used outside this module
+    helps to load the access tokens for the user to work on Twitter API
+    '''
+    with open("tokens.json", 'r') as f_read:
+        return json.load(f_read)
+
+@pytest.fixture(scope="session") # fixture scope is for this session
+def twitter_session():
+    '''
+    Load the session with user credentials -- access tokens.
+
+    Authentication method available:
+    Oauth1 - Oauth 1.0a has access to multiple endpoints including WRITE ops
+    Oauth2 - auth bearer header only has read access
+
+    As we will be needing to access endpoint for "statuses" WRITE operations,
+    Oauth1 session is created and used for futher for all the test cases.
+    '''
+    tokens = __load_tokens()
+    session = OAuth1Session(tokens['api_key'], client_secret=tokens['api_secret_key'],
+                            resource_owner_key=tokens['access_token'],
+                            resource_owner_secret=tokens['access_token_secret'])
+    ## OAuth1 is used for the Authentication
+    return session
